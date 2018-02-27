72dc6f3870994a708daaa3babfaf4321559513b9
pinfort <ptg@nijitei.com>
2018-02-27 18:07:31 +0900

    change auth path

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::group(
+    ['prefix' => 'auth'],
+    function () {
+        Auth::routes();
+    }
+);
 
 Route::get('/home', 'HomeController@index')->name('home');
