6db5785e31a39606ef43ebcb04f789d17a0e88c9
pinfort <ptg@nijitei.com>
2018-03-01 14:29:49 +0900

    apiファイル移動

--- resources/assets/js/components/api.js
+++ resources/assets/js/components/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
-import csrfToken from './csrf_token';
 
 export default () => axios.create({
-  headers: {
-    'X-CSRF-TOKEN': csrfToken,
-  },
-
   transformResponse: [(data) => {
     try {
       return JSON.parse(data);
