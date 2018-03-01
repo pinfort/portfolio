ddf4cb0dac0d642edab2343877d8c8fc4ba47fdc
pinfort <ptg@nijitei.com>
2018-03-01 14:51:55 +0900

    とりあえずproviderで囲う

--- resources/assets/js/features/app/containers/portfolio.js
+++ resources/assets/js/features/app/containers/portfolio.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
 export default class Portfolio extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
+            <Provider>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <div className="card">
+                                <div className="card-header">Example Component</div>
 
-                            <div className="card-body">
-                                I'm an example component!
+                                <div className="card-body">
+                                    I'm an example component!
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Provider>
         );
     }
 }
