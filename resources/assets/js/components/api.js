a2c8867770fecbf3454467904da8535f19170cc4
pinfort <ptg@nijitei.com>
2018-03-02 00:25:14 +0900

    インデント修正

--- resources/assets/js/components/api.js
+++ resources/assets/js/components/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default () => axios.create({
-  transformResponse: [(data) => {
-    try {
-      return JSON.parse(data);
-    } catch (Exception) {
-      return data;
-    }
-  }],
+    transformResponse: [(data) => {
+        try {
+            return JSON.parse(data);
+        } catch (Exception) {
+            return data;
+        }
+    }],
 });
