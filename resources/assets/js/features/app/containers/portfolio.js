d7860037ec45d0e8a4eac9d466656e8dcc158ba3
pinfort <ptg@nijitei.com>
2018-03-02 00:26:45 +0900

    storeを渡す。テーブルコンポーネント動作テスト

--- resources/assets/js/features/app/containers/portfolio.js
+++ resources/assets/js/features/app/containers/portfolio.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
+import { List, Map } from 'immutable';
 import { Provider } from 'react-redux';
+import SimpleTable from 'src/components/table/simple_table';
+import configureStore from 'src/store/configureStore';
+
+export const store = configureStore();
 
 export default class Portfolio extends Component {
+
     render() {
         return (
-            <Provider>
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-md-8">
-                            <div className="card">
-                                <div className="card-header">Example Component</div>
+            <Provider store={store}>
+                <div className='container'>
+                    <div className='row justify-content-center'>
+                        <div className='col-md-8'>
+                            <div className='card'>
+                                <div className='card-header'>Example Component</div>
 
-                                <div className="card-body">
-                                    I'm an example component!
+                                <div className='card-body'>
+                                    <SimpleTable
+                                        tid='main_table'
+                                        tclass='table'
+                                        thead={List.of(
+                                            List.of(
+                                                Map({ 'isTitle': true, 'isLink': false, 'txt': 'test title' })
+                                            ),
+                                            List.of(
+                                                Map({ 'isTitle': true, 'isLink': false, 'txt': 'test title2' })
+                                            )
+                                        )}
+                                        tbody={List.of(
+                                            List.of(
+                                                Map({ 'isTitle': false, 'isLink': false, 'txt': 'test body' })
+                                            ),
+                                            List.of(
+                                                Map({ 'isTitle': false, 'isLink': false, 'txt': 'test body2' })
+                                            )
+                                        )}
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -21,4 +46,5 @@ export default class Portfolio extends Component {
             </Provider>
         );
     }
+
 }
