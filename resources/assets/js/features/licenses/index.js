247bed8d0ff2be5fa340b5b5285ce92baddb033b
pinfort <ptg@nijitei.com>
2018-03-05 23:49:51 +0900

    Licensesコンポーネント追加

---
+++ resources/assets/js/features/licenses/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List, Map, fromJS } from 'immutable';
+import SimpleTable from 'src/components/table/simple_table';
+
+export default class Licenses extends React.Component {
+
+    static propTypes = {
+        table: PropTypes.arrayOf(
+            PropTypes.objectOf(
+                PropTypes.oneOftype([
+                    PropTypes.string,
+                    PropTypes.number,
+                ])
+            )
+        ),
+    }
+
+    static thead = List.of(
+        List.of(
+            Map({ 'isTitle': true, 'isLink': false, 'txt': '取得年月' }),
+            Map({ 'isTitle': true, 'isLink': false, 'txt': '資格' }),
+        )
+    );
+
+    render() {
+        const { table } = this.props;
+        let tbody = [];
+        table.forEach(row => {
+            let formatted_row = [];
+
+            // <1カラム目: 取得年月>
+            const get_at = row.get_at;
+            let get_year = get_at;
+            get_year.slice(0, -2);
+            let get_month = get_at;
+            get_month.slice(-2);
+            const get_at_str = get_year + '/' + get_month;
+            const get_at_obj = { 'isTitle': false, 'isLink': false, 'txt': get_at_str };
+            formatted_row.push(get_at_obj);
+            // </1カラム目: 取得年月>
+
+            // <2カラム目: 資格名>
+            // TODO: リンク追加可能にする
+            const name = row.name;
+            const name_obj = { 'isTitle': false, 'isLink': false, 'txt': name };
+            formatted_row.push(name_obj);
+            // </2カラム目: 資格名>
+
+            // 行を追加
+            tbody.push(formatted_row);
+        });
+        tbody = fromJS(tbody); // to immutable
+
+        return (
+            <SimpleTable tid='Licenses' tclass='table table-hover' thead={this.thead} tbody={tbody} />
+        );
+    }
+
+}
