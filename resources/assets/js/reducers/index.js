f2cc5a342fefc2fd5524f0e7f949ee8d80c013f7
pinfort <ptg@nijitei.com>
2018-03-08 02:37:38 +0900

    つくったリデューサーたちを有効化

--- resources/assets/js/reducers/index.js
+++ resources/assets/js/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux-immutable';
+import message from './message';
+import licenses from './licenses';
 
 const reducers = {
+    message,
+    licenses,
 };
 
 export default combineReducers(reducers);
