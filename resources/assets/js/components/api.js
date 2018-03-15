d5e628ffa1a18b29deae0ef1f75b6821becba742
pinfort <ptg@nijitei.com>
2018-03-15 17:04:01 +0900

    api送信時トークンを使う

--- resources/assets/js/components/api.js
+++ resources/assets/js/components/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import apiToken from './api_token';
 
 export default () => axios.create({
+    headers: {
+        'Authorization': 'Bearer ' + apiToken,
+    },
     transformResponse: [(data) => {
         try {
             return JSON.parse(data);
