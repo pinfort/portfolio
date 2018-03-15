4ca4fc81870b107d1eb7b2e41c40e4d488aa9039
pinfort <ptg@nijitei.com>
2018-03-15 18:49:28 +0900

    フォームをいじりやすく

--- resources/assets/js/components/form/simple_form.js
+++ resources/assets/js/components/form/simple_form.js
@@ -14,24 +14,41 @@ export default class SimpleForm extends React.Component {
         contents: ImmutablePropTypes.list.isRequired,
     }
 
+    onSubmit() {}
+
+    analyzeForm(contents, k) {
+        contents.map((content, i) => {
+            if (content.get('type') === 'text') {
+                return content.get('txt');
+            } else if (content.get('type') === 'button') {
+                return this.createButton(content, i, k);
+            } else if (content.get('type') === 'input'){
+                return this.createInput(content, i, k);
+            } else if (content.get('type') === 'font_awesome'){
+                return this.createFontAwesome(content, i, k);
+            }
+            return content;
+        });
+    }
+
+    createButton(content, i, k) {
+        return <SimpleButton key={k+ '_simple_button_' + i} k={k+ '_simple_button_' + i + '_child'} content={content} children={content.get('children')} />;
+    }
+
+    createInput(content, i, k) {
+        return <SimpleInput key={k + '_simple_input_' + i} k={k + '_simple_input_' + i + '_child'} content={content} />;
+    }
+
+    createFontAwesome(content, i, k) {
+        return <i key={k + '_font_awesome_' + i + '_' + content.get('className')} className={content.get('className')} area-hidden='true' />;
+    }
+
     render () {
         const { action, method, k, className, contents } = this.props;
 
         return (
-            <form action={action} method={method} key={k} className={className}>
-                {contents.map((content, i) => {
-                    if (content.get('type') === 'text') {
-                        return content.get('txt');
-                    } else if (content.get('type') === 'button') {
-                        return <SimpleButton key={k+ '_simple_button_' + i} k={k+ '_simple_button_' + i + '_child'} content={content} children={content.get('children')} />;
-                    } else if (content.get('type') === 'input'){
-                        return <SimpleInput key={k + '_simple_input_' + i} k={k + '_simple_input_' + i + '_child'} content={content} />;
-                    } else if (content.get('type') === 'font_awesome'){
-                        return <i key={k + '_font_awesome_' + i + '_' + content.get('className')} className={content.get('className')} area-hidden='true' />;
-                    } else {
-                        return content;
-                    }
-                })}
+            <form action={action} method={method} key={k} className={className} onSubmit={this.onSubmit()}>
+                {this.analyzeForm(contents, k)}
             </form>
         );
     }
