a153e8713bad5736953732e188c4d249d2ef448a
pinfort <ptg@nijitei.com>
2018-03-15 23:28:23 +0900

    adminスキルJS側ルート追加

--- resources/assets/js/features/app/components/portfolio.js
+++ resources/assets/js/features/app/components/portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import HomePage from 'src/features/home_page';
 import LicensesPage from 'src/features/admin/licenses_page';
+import SkillsPage from 'src/features/admin/skills_page';
 import laroute from 'src/laroute.js';
 
 export default class Portfolio extends React.Component {
@@ -11,6 +12,7 @@ export default class Portfolio extends React.Component {
             <Switch>
                 <Route path={laroute.route('home')} component={HomePage} />
                 <Route path={laroute.route('admin_licenses')} component={LicensesPage} />
+                <Route path={laroute.route('admin_skills')} component={SkillsPage} />
             </Switch>
         );
     }
