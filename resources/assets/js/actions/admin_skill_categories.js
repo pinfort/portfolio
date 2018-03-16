55bd11c4bbe004652099b06e018c2a001acc982f
pinfort <ptg@nijitei.com>
2018-03-16 14:40:58 +0900

    スキルカテゴリアクション、リデューサー作成

--- resources/assets/js/actions/admin_skill_categories.js
+++ resources/assets/js/actions/admin_skill_categories.js
@@ -37,3 +37,86 @@ export function refreshSkillCategoriesFail(error) {
         error,
     };
 }
+
+export const ADMIN_SKILL_CATEGORY_DELETE_REQUEST = 'ADMIN_SKILL_CATEGORY_DELETE_REQUEST';
+export const ADMIN_SKILL_CATEGORY_DELETE_SUCCESS = 'ADMIN_SKILL_CATEGORY_DELETE_SUCCESS';
+export const ADMIN_SKILL_CATEGORY_DELETE_FAIL    = 'ADMIN_SKILL_CATEGORY_DELETE_FAIL';
+
+export function deleteSkillCategory(id) {
+    return (dispatch, getState) => {
+        dispatch(deleteSkillCategoryRequest());
+
+        api(getState).delete(
+            '/api/skills/categories/' + id,
+        ).then(response => {
+            dispatch(deleteSkillCategorySuccess(response.data.data.id));
+        }).catch(error => {
+            dispatch(deleteSkillCategoryFail(error));
+        });
+    };
+}
+
+export function deleteSkillCategoryRequest() {
+    return {
+        type: ADMIN_SKILL_CATEGORY_DELETE_REQUEST,
+    };
+}
+
+export function deleteSkillCategorySuccess(deletedSkillCategoryId) {
+    return {
+        type: ADMIN_SKILL_CATEGORY_DELETE_SUCCESS,
+        deletedSkillCategoryId,
+    };
+}
+
+export function deleteSkillCategoryFail(error) {
+    return {
+        type: ADMIN_SKILL_CATEGORY_DELETE_FAIL,
+        error,
+    };
+}
+
+export const ADMIN_SKILL_CATEGORY_ADD_REQUEST = 'ADMIN_SKILL_CATEGORY_ADD_REQUEST';
+export const ADMIN_SKILL_CATEGORY_ADD_SUCCESS = 'ADMIN_SKILL_CATEGORY_ADD_SUCCESS';
+export const ADMIN_SKILL_CATEGORY_ADD_FAIL    = 'ADMIN_SKILL_CATEGORY_ADD_FAIL';
+
+export function addSkillCategory(data) {
+    return (dispatch, getState) => {
+        dispatch(addSkillCategoryRequest());
+
+        let params = new URLSearchParams();
+
+        for(let k of Object.keys(data)) {
+            params.append(k, data[k]);
+        }
+
+        api(getState).post(
+            '/api/skills/categories',
+            params,
+        ).then(response => {
+            dispatch(addSkillCategorySuccess(response.data.data.skill_category));
+        }).catch(error => {
+            dispatch(addSkillCategoryFail(error));
+        });
+    };
+}
+
+export function addSkillCategoryRequest() {
+    return {
+        type: ADMIN_SKILL_CATEGORY_ADD_REQUEST,
+    };
+}
+
+export function addSkillCategorySuccess(addedSkillCategory) {
+    return {
+        type: ADMIN_SKILL_CATEGORY_ADD_SUCCESS,
+        addedSkillCategory,
+    };
+}
+
+export function addSkillCategoryFail(error) {
+    return {
+        type: ADMIN_SKILL_CATEGORY_ADD_FAIL,
+        error,
+    };
+}
--- resources/assets/js/reducers/admin_skill_categories.js
+++ resources/assets/js/reducers/admin_skill_categories.js
@@ -2,11 +2,34 @@ import {
     ADMIN_SKILL_CATEGORIES_REFRESH_REQUEST,
     ADMIN_SKILL_CATEGORIES_REFRESH_SUCCESS,
     ADMIN_SKILL_CATEGORIES_REFRESH_FAIL,
+    ADMIN_SKILL_CATEGORY_DELETE_REQUEST,
+    ADMIN_SKILL_CATEGORY_DELETE_SUCCESS,
+    ADMIN_SKILL_CATEGORY_DELETE_FAIL,
+    ADMIN_SKILL_CATEGORY_ADD_REQUEST,
+    ADMIN_SKILL_CATEGORY_ADD_SUCCESS,
+    ADMIN_SKILL_CATEGORY_ADD_FAIL,
 } from 'src/actions/admin_skill_categories';
 import { Map, fromJS } from 'immutable';
 
 function normalizeSkillCategories(state, skillCategoryList) {
     return state.set('skill_category', fromJS(skillCategoryList));
+}
+
+function deleteSkillCategory(state, skillCategoryId) {
+    let newState = [];
+    state.get('skill_category').map((skillCategory) => {
+
+        if (skillCategory.get('id') !== skillCategoryId) {
+            newState.push(skillCategory);
+        }
+    });
+    return state.set('skill_category', fromJS(newState));
+}
+
+function addSkillCategory(state, skill_category) {
+    let newState = state.get('skill_category').toJS();
+    newState.push(skill_category);
+    return state.set('skill_category', fromJS(newState));
 }
 
 const initialState = Map({
@@ -17,6 +40,14 @@ export default function admin_skills(state = initialState, action) {
     switch (action.type) {
     case ADMIN_SKILL_CATEGORIES_REFRESH_SUCCESS:
         return normalizeSkillCategories(state, action.skillCategoryList);
+    case ADMIN_SKILL_CATEGORY_DELETE_SUCCESS:
+        return deleteSkillCategory(state, action.deletedSkillCategoryId);
+    case ADMIN_SKILL_CATEGORY_ADD_SUCCESS:
+        return addSkillCategory(state, action.addedSkillCategory);
+    case ADMIN_SKILL_CATEGORY_ADD_REQUEST:
+    case ADMIN_SKILL_CATEGORY_ADD_FAIL:
+    case ADMIN_SKILL_CATEGORY_DELETE_REQUEST:
+    case ADMIN_SKILL_CATEGORY_DELETE_FAIL:
     case ADMIN_SKILL_CATEGORIES_REFRESH_REQUEST:
     case ADMIN_SKILL_CATEGORIES_REFRESH_FAIL:
     default:
