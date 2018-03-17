81be95202becfd871c35fa8fc982c6d36a00ad88
pinfort <ptg@nijitei.com>
2018-03-17 15:32:01 +0900

    スキルページ、ライセンスページ有効化

--- resources/assets/js/features/app/components/portfolio.js
+++ resources/assets/js/features/app/components/portfolio.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import HomePage from 'src/features/home_page';
-import LicensesPage from 'src/features/admin/licenses_page';
-import SkillsPage from 'src/features/admin/skills_page';
+import LicensesPage from 'src/features/licenses_page';
+import SkillsPage from 'src/features/skills_page';
+import AdminLicensesPage from 'src/features/admin/licenses_page';
+import AdminSkillsPage from 'src/features/admin/skills_page';
 import SkillCategoriesPage from 'src/features/admin/skill_categories_page';
 import laroute from 'src/laroute.js';
 
@@ -12,8 +14,10 @@ export default class Portfolio extends React.Component {
         return (
             <Switch>
                 <Route path={laroute.route('home')} component={HomePage} />
-                <Route path={laroute.route('admin_licenses')} component={LicensesPage} />
-                <Route exact path={laroute.route('admin_skills')} component={SkillsPage} />
+                <Route path={laroute.route('licenses')} component={LicensesPage} />
+                <Route path={laroute.route('skills')} component={SkillsPage} />
+                <Route path={laroute.route('admin_licenses')} component={AdminLicensesPage} />
+                <Route exact path={laroute.route('admin_skills')} component={AdminSkillsPage} />
                 <Route path={laroute.route('admin_skill_categories')} component={SkillCategoriesPage} />
             </Switch>
         );
