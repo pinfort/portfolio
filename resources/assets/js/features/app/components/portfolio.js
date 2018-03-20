973f67c702208b840a36da19fcebd014afeaf1f9
pinfort <ptg@nijitei.com>
2018-03-20 18:28:06 +0900

    Admin worksルート追加

--- resources/assets/js/features/app/components/portfolio.js
+++ resources/assets/js/features/app/components/portfolio.js
@@ -5,7 +5,8 @@ import LicensesPage from 'src/features/licenses_page';
 import SkillsPage from 'src/features/skills_page';
 import AdminLicensesPage from 'src/features/admin/licenses_page';
 import AdminSkillsPage from 'src/features/admin/skills_page';
-import SkillCategoriesPage from 'src/features/admin/skill_categories_page';
+import AdminSkillCategoriesPage from 'src/features/admin/skill_categories_page';
+import AdminWorksPage from 'src/features/admin/works_page';
 import laroute from 'src/laroute.js';
 
 export default class Portfolio extends React.Component {
@@ -18,7 +19,8 @@ export default class Portfolio extends React.Component {
                 <Route path={laroute.route('skills')} component={SkillsPage} />
                 <Route path={laroute.route('admin_licenses')} component={AdminLicensesPage} />
                 <Route exact path={laroute.route('admin_skills')} component={AdminSkillsPage} />
-                <Route path={laroute.route('admin_skill_categories')} component={SkillCategoriesPage} />
+                <Route exact path={laroute.route('admin_skill_categories')} component={AdminSkillCategoriesPage} />
+                <Route path={laroute.route('admin_works')} component={AdminWorksPage} />
             </Switch>
         );
     }
