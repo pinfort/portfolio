92090830b519700d2d0f1f2b3710573e8157ff9c
pinfort <ptg@nijitei.com>
2018-03-20 18:27:22 +0900

    /homeにworks追加

--- resources/assets/js/features/home_page/index.js
+++ resources/assets/js/features/home_page/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Tabs from 'src/features/home_page/tabs';
 import Skills from 'src/features/skills';
 import Licenses from 'src/features/licenses';
+import Works from 'src/features/works';
 
 export default class HomePage extends React.Component {
 
@@ -14,6 +15,7 @@ export default class HomePage extends React.Component {
                         <div className='tab-pane active'>
                             <Skills />
                             <Licenses />
+                            <Works />
                         </div>
                     </div>
                 </div>
