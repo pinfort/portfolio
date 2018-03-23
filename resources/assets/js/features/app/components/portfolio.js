9b2e26c7668e6ecf8cd4b36a14afae693d00cf9e
pinfort <ptg@nijitei.com>
2018-03-23 11:13:30 +0900

    サービスとアカウントのページ追加

--- resources/assets/js/features/app/components/portfolio.js
+++ resources/assets/js/features/app/components/portfolio.js
@@ -7,6 +7,8 @@ import AdminLicensesPage from 'src/features/admin/licenses_page';
 import AdminSkillsPage from 'src/features/admin/skills_page';
 import AdminSkillCategoriesPage from 'src/features/admin/skill_categories_page';
 import AdminWorksPage from 'src/features/admin/works_page';
+import AdminServicesPage from 'src/features/admin/services_page';
+import AdminAccountsPage from 'src/features/admin/accounts_page';
 import WorksPage from 'src/features/works_page';
 import laroute from 'src/laroute.js';
 
@@ -23,6 +25,8 @@ export default class Portfolio extends React.Component {
                 <Route exact path={laroute.route('admin_skills')} component={AdminSkillsPage} />
                 <Route exact path={laroute.route('admin_skill_categories')} component={AdminSkillCategoriesPage} />
                 <Route path={laroute.route('admin_works')} component={AdminWorksPage} />
+                <Route path={laroute.route('admin_services')} component={AdminServicesPage} />
+                <Route path={laroute.route('admin_accounts')} component={AdminAccountsPage} />
             </Switch>
         );
     }
