addb777507a50ae7875e4b6113803504b8e11664
pinfort <ptg@nijitei.com>
2018-03-23 12:47:02 +0900

    登録不可のエラーメッセージが表示されないようになってたので直す

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (Session::has('message'))
+                <div class="alert alert-danger">{{ Session::get('message') }}</div>
+            @endif
             <div class="card">
                 <div class="card-header">Login</div>
 
