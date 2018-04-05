90149e8a3dc39f0e74c655e5b28d6e99f11132df
pinfort <ptg@nijitei.com>
2018-04-06 00:51:19 +0900

    nodefault introduction is bad

--- database/migrations/2018_04_01_162918_add_intro_column_to_users_table.php
+++ database/migrations/2018_04_01_162918_add_intro_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddIntroColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->longText('introduction');
+            $table->string('introduction')->default('');
         });
     }
 
