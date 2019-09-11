8aaa16fd52299e8648cd18854101ea6749df8335
pinfort <ptg@nijitei.com>
2019-09-12 00:19:16 +0900

    change list

--- resources/js/features/accounts/components/accounts.js
+++ resources/js/features/accounts/components/accounts.js
@@ -92,6 +92,7 @@ export default class Accounts extends React.Component {
                                 <SimpleTable key={'accounts_' + e[0] + '_table_wrap'} tid={'accounts_' + e[0] + '_table'} tclass='table table-hover' thead={this.thead} tbody={e[1]} />,
                             ])
                         }
+                        l_class='my-1'
                     />
                 </span>
             )
