a73d23678dd03172905e22d3d3d619c058bc85cc
pinfort <ptg@nijitei.com>
2019-09-27 16:29:40 +0900

    testing

--- app/Console/Commands/Deploy.php
+++ app/Console/Commands/Deploy.php
@@ -82,9 +82,7 @@ class Deploy extends Command
 
         $this->info('building javaScript');
         exec('yarn run prod', $out, $return_var);
-        foreach ($out as $outline) {
-            $this->info($outline);
-        }
+        echo $return_var;
         if ($return_var !== 0) {
             $this->error('failed to build javaScript');
             unset($out);
