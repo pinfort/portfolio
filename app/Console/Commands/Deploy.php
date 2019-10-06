b758f425e96dba3e7f116fcedf8775c725154fac
pinfort <ptg@nijitei.com>
2019-10-06 17:52:15 +0900

    test

--- app/Console/Commands/Deploy.php
+++ app/Console/Commands/Deploy.php
@@ -89,7 +89,7 @@ class Deploy extends Command
         }
 
         $this->info('building javaScript');
-        exec('yarn run prod', $out, $return_var);
+        exec('/home/nijitei/opt/yarn/bin/yarn run prod', $out, $return_var);
         if ($return_var !== 0) {
             $this->error('failed to build javaScript');
             foreach ($out as $line) {
