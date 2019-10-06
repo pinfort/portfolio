66cc8c5b11b67053baa26df70f8f2a9ef3da7cb9
pinfort <ptg@nijitei.com>
2019-10-06 17:20:17 +0900

    change mitame

--- resources/js/features/works/components/works.js
+++ resources/js/features/works/components/works.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { List, Map, fromJS } from 'immutable';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import SimpleCard from '../../../components/cards/simple_card';
+import {Link} from "react-router-dom";
 
 export default class Works extends React.Component {
 
@@ -20,13 +21,9 @@ export default class Works extends React.Component {
         let cards = [];
         if (works !== undefined && works !== null) {
             works.map(row => {
-                let formatted_card = {};
-                let formatted_card_body = [];
-
                 // <名称>
                 const name = row.get('name');
-                const name_obj = { type: 'title', txt: name };
-                formatted_card_body.push(name_obj);
+                const card_name = (<h5 key={'work_card_title'} className='card-title'>{name}</h5>);
                 // </名称>
 
                 // <説明>
@@ -34,26 +31,39 @@ export default class Works extends React.Component {
                 if (description.length > 50) {
                     description = description.slice(0, 50) + '...';
                 }
-                const description_obj = { type: 'txt', 'txt': description };
-                formatted_card_body.push(description_obj);
+                const card_desc = (<p key={'work_card_text'} className='card-text'>{description}</p>);
                 // </説明>
 
                 // <詳細ページ>
                 const id = row.get('id');
-                let url_obj = {};
-                url_obj = { type: 'link', txt: '詳しく', 'link': '/works/' + id };
-                formatted_card_body.push(url_obj);
+                const card_link = (<Link key={'work_card_link'} className={'btn btn-primary'} to={'/works/' + id}>{'詳しく'}</Link>);
                 // </詳細ページ>
 
-                formatted_card.body = formatted_card_body;
-                cards.push(formatted_card);
+                const card_body = (
+                    <div className='card-body' key={'work_card_body'}>
+                        <div>
+                            {card_name}
+                            {card_desc}
+                        </div>
+                        {card_link}
+                    </div>
+                );
+                cards.push(
+                    <div className={'card mx-2 mb-3'} style={{ width: '15rem' }}>
+                        {card_body}
+                    </div>
+                );
             });
         } else {
-            cards.push({
-                body: [
-                    { type: 'txt', 'txt': 'None' },
-                ],
-            });
+            cards.push(<div className={'card mx-2 mb-3'} style={{ width: '15rem' }}>
+                <div className='card-body' key={'work_card_body'}>
+                    <div>
+                        <h5 key={'work_card_title'} className='card-title'>none</h5>
+                        <p key={'work_card_text'} className='card-text'>desc</p>
+                    </div>
+                    <Link key={'work_card_link'} className={'btn btn-primary disabled'} to={''}>{'詳しく'}</Link>
+                </div>
+            </div>);
         }
 
         cards = fromJS(cards); // to immutable
@@ -62,9 +72,7 @@ export default class Works extends React.Component {
             <div className='m-3'>
                 <h3 className='text-center'>Works</h3>
                 <div id='works-body' key={'main_works'} className='card-body flex-wrap' style={{ display: 'flex' }}>
-                    {cards.map((card, i) => {
-                        return <SimpleCard key={'work_card_' + i + '_wrap'} cid={'work_card_' + i} card={card} />;
-                    })}
+                    {cards}
                 </div>
             </div>
         );
