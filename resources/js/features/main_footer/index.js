d377a0488ad574c686b00e77aa87d7dfbe60d5b4
pinfort <ptg@nijitei.com>
2019-12-15 23:33:34 +0900

    fix class name

--- resources/js/features/main_footer/index.js
+++ resources/js/features/main_footer/index.js
@@ -5,9 +5,9 @@ export default class MainFooter extends React.Component {
     render() {
         return (
             <footer>
-                <nav class="navbar navbar-expand-md navbar-light">
-                    <ul class="nav navbar-nav ml-auto mr-auto">
-                        <li class="nav-item">
+                <nav className="navbar navbar-expand-md navbar-light">
+                    <ul className="nav navbar-nav ml-auto mr-auto">
+                        <li className="nav-item">
                             <Link className="nav-link" key={'privacy_link'} to={'/privacy'}>訪問者のプライバシーに関する情報</Link>
                         </li>
                     </ul>
